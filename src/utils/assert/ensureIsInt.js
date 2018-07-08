092fad0d3bc4433bce01ea6e6d0898e742b7122e
Yan Nunes <yannunes@gmail.com>
2018-07-08 13:42:59 +0200

    Utils

--- src/utils/assert/ensureIsInt.js
+++
@@ -1,9 +0,0 @@
-module.exports = function ensureIsInt(number, defaultValue, customCondition = () => true) {
-    const parsed = parseInt(number)
-
-    if (Number.isNaN(parsed) || !customCondition(parsed)) {
-        return defaultValue
-    }
-
-    return parsed
-}
